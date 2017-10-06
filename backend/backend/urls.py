cdbe2e302bbd43792571ed51bcaaf9cb146b618c
Sergio Garcia <srgarcia91@gmail.com>
2017-10-06 20:52:56 +0200

    jwt token created

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^auth/$', obtain_jwt_token),
 ]
