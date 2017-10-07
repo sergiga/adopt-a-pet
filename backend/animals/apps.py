f7e741baf7ee5f92da75c78a1bf7687027b3b0ab
Sergio Garcia <srgarcia91@gmail.com>
2017-10-07 17:29:53 +0200

    Created the Animal model

--- backend/animals/apps.py
+++ backend/animals/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class AnimalsConfig(AppConfig):
     name = 'animals'
--- backend/animals/models.py
+++ backend/animals/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    age = models.FloatField(blank=False)
+    description = models.TextField(blank=False)
+    owner = models.ForeignKey('auth.User', related_name='owners', on_delete=models.CASCADE)
+    adopter = models.ForeignKey('auth.User', related_name='adopters')
+
+class AnimalType(models.Model):
+    name = models.CharField(max_length=100, blank=False)
