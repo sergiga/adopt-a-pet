672b8114a57b825a3bcb73cede9629762ab26056
Sergio Garcia <srgarcia91@gmail.com>
2017-10-07 18:32:17 +0200

    Animal model updated

--- backend/animals/models.py
+++ backend/animals/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
+class AnimalType(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return '%s' % (self.name)
+
 class Animal(models.Model):
     name = models.CharField(max_length=100, blank=False)
     age = models.FloatField(blank=False)
     description = models.TextField(blank=False)
     owner = models.ForeignKey('auth.User', related_name='owners', on_delete=models.CASCADE)
-    adopter = models.ForeignKey('auth.User', related_name='adopters')
-
-    def __str__(self):
-        return '%s' % (self.name)
-
-class AnimalType(models.Model):
-    name = models.CharField(max_length=100, blank=False)
+    adopter = models.ForeignKey('auth.User', related_name='adopters', null=True)
+    animal_type = models.ForeignKey(AnimalType, related_name='animals', null=True)
 
     def __str__(self):
         return '%s' % (self.name)
