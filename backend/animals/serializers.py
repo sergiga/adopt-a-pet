3bfc4515681ca7798f9c38c5ffb190eea90ac1ac
Sergio Garcia <srgarcia91@gmail.com>
2017-10-07 18:32:51 +0200

    Added animal_type in AnimalSerializer

--- backend/animals/serializers.py
+++ backend/animals/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 from animals.models import Animal
 
 class AnimalSerializer(serializers.ModelSerializer):
-    type = serializers.StringRelatedField()
+    animal_type = serializers.StringRelatedField()
 
     class Meta:
         model = Animal
-        fields = ('id', 'name', 'age', 'description'.
-                  'owner', 'adopter', 'type')
+        fields = ('id', 'name', 'age', 'description', 
+                  'owner', 'adopter', 'animal_type')
