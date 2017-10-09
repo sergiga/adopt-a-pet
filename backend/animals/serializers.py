ae45a0cb167c0cac53699ad7f76a0fdcb9bab8f6
Sergio Garcia <srgarcia91@gmail.com>
2017-10-09 19:06:21 +0200

    Adding animal_type slug related field to the animal serializer

--- backend/animals/serializers.py
+++ backend/animals/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
-from animals.models import Animal
+from animals.models import Animal, AnimalType
 
 class AnimalSerializer(serializers.ModelSerializer):
-    animal_type = serializers.StringRelatedField()
+    animal_type = serializers.SlugRelatedField(
+        slug_field='name', 
+        queryset = AnimalType.objects.all()
+    )
 
     class Meta:
         model = Animal
