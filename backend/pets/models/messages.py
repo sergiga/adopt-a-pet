6ec1e363090573dee680aa2a13a49d116ea6dc94
Sergio Garcia <srgarcia91@gmail.com>
2017-10-10 20:44:29 +0200

    Created Message model

---
+++ backend/pets/models/messages.py
@@ -0,0 +1,8 @@
+from django.db import models
+from pets.models.adoptions import Adoption
+
+class Message(models.Model):
+    content = models.CharField(max_length=140)
+    author = models.ForeignKey('auth.User', related_name='messages')
+    adoption = models.ForeignKey(Adoption, related_name='messages')
+    received_at = models.DateTimeField(auto_now=True, auto_now_add=False)
