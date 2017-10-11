5e948c119b9a3826db34cc5956f2fc9d2850adfa
Sergio Garcia <srgarcia91@gmail.com>
2017-10-11 19:58:06 +0200

    Changing the signal location to the model

--- backend/pets/models/adoptions.py
+++ backend/pets/models/adoptions.py
@@ -1,3 +1,4 @@
+from django.db.models.signals import post_save
 from django.db import models
 from pets.models.pets import Pet
 
@@ -14,3 +15,15 @@ class Adoption(models.Model):
     canceled_by_owner = models.BooleanField(blank=False, default=False)
     canceled_by_adopter = models.BooleanField(blank=False, default=False)
     updated_at = models.DateField(auto_now=True, auto_now_add=False)
+
+def update_adoptions(sender, **kwargs):
+    instance = kwargs['instance']
+    created = kwargs['created']
+    raw = kwargs['raw']
+
+    if not created and not raw:
+        for adoption in instance.adoptions.filter(status__name='Pending'):
+            adoption.canceled_by_owner = instance.in_adoption
+            adoption.save()
+    
+post_save.connect(update_adoptions, sender=Pet)
--- backend/pets/serializers/adoptions.py
+++ backend/pets/serializers/adoptions.py
@@ -1,9 +1,7 @@
-from django.db.models.signals import post_save
 from rest_framework import serializers
 from pets.models.adoptions import Adoption, AdoptionStatus
 from pets.serializers.pets import PetReadSerializer
 from pets.serializers.users import UserSerializer
-from pets.models.pets import Pet
 
 class AdoptionSerializer(serializers.ModelSerializer):
     status = serializers.SlugRelatedField(
@@ -18,15 +16,3 @@ class AdoptionSerializer(serializers.ModelSerializer):
 class AdoptionReadSerializer(AdoptionSerializer):
     pet = PetReadSerializer(read_only=True)
     adopter = UserSerializer(read_only=True)
-
-def update_adoptions(sender, **kwargs):
-    instance = kwargs['instance']
-    created = kwargs['created']
-    raw = kwargs['raw']
-
-    if not created and not raw:
-        for adoption in instance.adoptions.filter(status__name='Pending'):
-            adoption.canceled_by_owner = instance.in_adoption
-            adoption.save()
-    
-post_save.connect(update_adoptions, sender=Pet)
