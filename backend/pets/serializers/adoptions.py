10d20d3c5a555e00024fa64f207190ef82e86847
Sergio Garcia <srgarcia91@gmail.com>
2017-10-11 19:50:22 +0200

    If the pet in_adoption boolean changes, update all its adoptions

--- backend/pets/serializers/adoptions.py
+++ backend/pets/serializers/adoptions.py
@@ -1,7 +1,9 @@
+from django.db.models.signals import post_save
 from rest_framework import serializers
 from pets.models.adoptions import Adoption, AdoptionStatus
 from pets.serializers.pets import PetReadSerializer
 from pets.serializers.users import UserSerializer
+from pets.models.pets import Pet
 
 class AdoptionSerializer(serializers.ModelSerializer):
     status = serializers.SlugRelatedField(
@@ -10,8 +12,21 @@ class AdoptionSerializer(serializers.ModelSerializer):
     )
     class Meta:
         model = Adoption
-        fields = ('id', 'status', 'pet', 'adopter', 'updated_at')
+        fields = ('id', 'status', 'pet', 'adopter',
+                  'canceled_by_owner', 'canceled_by_adopter', 'updated_at')
 
 class AdoptionReadSerializer(AdoptionSerializer):
     pet = PetReadSerializer(read_only=True)
     adopter = UserSerializer(read_only=True)
+
+def update_adoptions(sender, **kwargs):
+    instance = kwargs['instance']
+    created = kwargs['created']
+    raw = kwargs['raw']
+
+    if not created and not raw:
+        for adoption in instance.adoptions.filter(status__name='Pending'):
+            adoption.canceled_by_owner = instance.in_adoption
+            adoption.save()
+    
+post_save.connect(update_adoptions, sender=Pet)
