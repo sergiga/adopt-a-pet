1fa4ff09b9a5a7ecc5faac4868c18f55b954156b
Sergio Garcia <srgarcia91@gmail.com>
2017-10-11 19:02:42 +0200

    Adding the status to the AdoptionSerializer

--- backend/pets/serializers/adoptions.py
+++ backend/pets/serializers/adoptions.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
-from pets.models.adoptions import Adoption
+from pets.models.adoptions import Adoption, AdoptionStatus
 from pets.serializers.pets import PetReadSerializer
 from pets.serializers.users import UserSerializer
 
 class AdoptionSerializer(serializers.ModelSerializer):
+    status = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset = AdoptionStatus.objects.all(),
+    )
     class Meta:
         model = Adoption
-        fields = ('id', 'pet', 'adopter', 'updated_at')
+        fields = ('id', 'status', 'pet', 'adopter', 'updated_at')
 
 class AdoptionReadSerializer(AdoptionSerializer):
     pet = PetReadSerializer(read_only=True)
