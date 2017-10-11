85c4f73704100292e3049716e155ab192fb5aa71
Sergio Garcia <srgarcia91@gmail.com>
2017-10-11 19:49:53 +0200

    Added canceled status for both owner and adopter to the adoption

--- backend/pets/models/adoptions.py
+++ backend/pets/models/adoptions.py
@@ -11,4 +11,6 @@ class Adoption(models.Model):
     status = models.ForeignKey(AdoptionStatus, related_name='adoptions', default=2)
     pet = models.ForeignKey(Pet, related_name='adoptions', on_delete=models.CASCADE)
     adopter = models.ForeignKey('auth.User', related_name='adoptions', null=True)
+    canceled_by_owner = models.BooleanField(blank=False, default=False)
+    canceled_by_adopter = models.BooleanField(blank=False, default=False)
     updated_at = models.DateField(auto_now=True, auto_now_add=False)
