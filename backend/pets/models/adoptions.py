c4c8bc410bdac4d335e474f09da57153b4278965
Sergio Garcia <srgarcia91@gmail.com>
2017-10-11 18:18:25 +0200

    Some model drops on cascade

--- backend/pets/models/adoptions.py
+++ backend/pets/models/adoptions.py
@@ -2,6 +2,6 @@ from django.db import models
 from pets.models.pets import Pet
 
 class Adoption(models.Model):
-    pet = models.ForeignKey(Pet, related_name='adoptions')
+    pet = models.ForeignKey(Pet, related_name='adoptions', on_delete=models.CASCADE)
     adopter = models.ForeignKey('auth.User', related_name='adoptions', null=True)
     updated_at = models.DateField(auto_now=True, auto_now_add=False)
--- backend/pets/models/messages.py
+++ backend/pets/models/messages.py
@@ -4,5 +4,5 @@ from pets.models.adoptions import Adoption
 class Message(models.Model):
     content = models.CharField(max_length=140)
     author = models.ForeignKey('auth.User', related_name='messages')
-    adoption = models.ForeignKey(Adoption, related_name='messages')
+    adoption = models.ForeignKey(Adoption, related_name='messages', on_delete=models.CASCADE)
     received_at = models.DateTimeField(auto_now=True, auto_now_add=False)
